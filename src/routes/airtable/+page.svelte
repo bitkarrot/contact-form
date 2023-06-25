a8bc1ba40bf7538f85858f3c8f70e28d7e49186a
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-06-24 20:15:50 -0700

    reorder pages

---
+++ src/routes/airtable/+page.svelte
@@ -0,0 +1,16 @@
+<section> 
+
+<h1>Contact Form to Airtable</h1>
+
+<p> Wait for page to load.....</p>
+
+<iframe
+	title="Airtable Form"
+	class="airtable-embed"
+	src="https://airtable.com/embed/shrNpNB05tDklr03p"
+	frameborder="0"
+	width="100%"
+	height="533"
+	style="background: transparent; border: 1px solid #ccc;"
+/>
+</section>
